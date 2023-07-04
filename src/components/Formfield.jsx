80c7af03765970ae0ad0e3d6899e8aa719d5478a
Ashutosh <thisisashutoshdubey@gmail.com>
2023-07-04 17:56:50 +0530

    completed the frontend part and now starting the backend

--- src/components/Formfield.jsx
+++ src/components/Formfield.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
 
-const Formfield = () => {
+const Formfield = ({
+  label,
+  type,
+  name,
+  placeholder,
+  value,
+  handleChange,
+  isSurpriseMe,
+  handleSurpriseMe,
+}) => {
   return (
-    <div>Formfield</div>
-  )
-}
+    <div>
+      <div className="flex items-center gap-2 mb-2 ">
+        <label
+          htmlFor={name}
+          className="block text-sm font-medium text-gray-900"
+        >
+          {label}
+        </label>
+        {isSurpriseMe && (
+          <button
+            type="button"
+            onClick={handleSurpriseMe}
+            className="font-semibold text-xs bg-[#ECECF1] text-black px-2 py-1 rounded-md"
+          >
+            Surprise Me
+          </button>
+        )}
+      </div>
+      <input
+        type={type}
+        name={name}
+        id={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded focus:ring-[#4649ff] focus:border-[#4649ff]
+         outline-none block w-full p-3 "
+      />
+    </div>
+  );
+};
 
-export default Formfield
+export default Formfield;
